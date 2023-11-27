ae2c505e495936a3bda88629b311ac557dc8a510
andy0427s <andy0427s@gmail.com>
2023-11-27 21:11:35 +0000

    Finished the experience part

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './ExperienceStyles';
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiences } from "../../constants/constants";
@@ -12,35 +10,50 @@ const ExperienceCard = ({ experience }) => {
         contentStyle={{
           background: "#1d1836",
           color: "#fff",
+        //   width: '400px',
+        //   minHeight: '600px'
         }}
-        contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-        date={experience.date}
-        iconStyle={{ background: experience.iconBg }}
+        contentArrowStyle={{ borderRight: "7px solid #232631" }}
+        date={
+            <div style={{ fontSize: '24px', marginRight:"24px", marginLeft:"24px"}}> {/* Adjust fontSize and color as needed */}
+              {experience.date}
+            </div>
+          }
+        iconStyle={{ background: experience.iconBg, width: '60px', height:'60px' }}
         icon={
-          <div className='flex justify-center items-center w-full h-full'>
+          <div style={{
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            width: '100%', 
+            height: '100%'
+          }}>
             <img
               src={experience.icon}
               alt={experience.company_name}
-              className='w-[60%] h-[60%] object-contain'
+              style={{
+                maxWidth: '100%', 
+                maxHeight: '100%', 
+                objectFit: 'contain',
+              }}
             />
           </div>
         }
       >
         <div>
-          <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
-          <p
-            className='text-secondary text-[16px] font-semibold'
-            style={{ margin: 0 }}
-          >
+          <h3 style={{ color: 'white', fontSize: '30px', fontWeight: 'bold' }}>
+            {experience.title}
+          </h3>
+          <p style={{ color: '#d1d5db', fontSize: '24px', fontWeight: '600', margin: 0 }}>
             {experience.company_name}
           </p>
         </div>
   
-        <ul className='mt-5 list-disc ml-5 space-y-2'>
+        <ul style={{ marginTop: '10px', listStyleType: 'disc', marginLeft: '0px', gap: '8px' }}>
           {experience.points.map((point, index) => (
             <li
               key={`experience-point-${index}`}
-              className='text-white-100 text-[14px] pl-1 tracking-wider'
+              style={{ color: 'white', fontSize: '24px', paddingLeft: '4px', letterSpacing: 'wider' }}
             >
               {point}
             </li>
@@ -58,44 +71,16 @@ const Experience = () =>  (
     I am a Software Engineer Placement at Lifecycle Software Ltd since September 2023. 
     Prior to this, I was a Software Engineer Intern at Therapyworks (Feb-Apr 2023), and I have MSc Computing degree from Cardiff University, which has provided a strong foundation for my technical skills and software development knowledge.
     </SectionText>
-    <List>
-      <ListItem>
-        <picture>
-          <DiReact size="3rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>Front-End</ListTitle>
-          <ListParagraph>
-            Experiece with <br />
-            React.js
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <DiFirebase size="3rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>Back-End</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            Node and Databases
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <DiZend size="3rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>UI/UX</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            tools like Figma
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-    </List>
+    <div style={{ marginTop: '5rem', display: 'flex', flexDirection: 'column' }}>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
+      </div>
   </Section>
 );
 
